ab997418025b05e5bce8e2f0e8bd751dbc23ce43
Potandr1977 <andrew.potapov.77@inbox.ru>
2025-01-31 21:55:55 +0300

    Добавил комментариев.

--- main.py
+++ main.py
@@ -15,15 +15,19 @@ def step(x):
         return 0
 
 def prediction(weights, bias, features):
-    return step(score(weights, bias, features))
+    scored = score(weights, bias, features)
+    return step(scored)
 
+# оцениваем ошибку
 def error(weights, bias, features, label):
     pred = prediction(weights, bias, features)
     if pred == label:
         return 0
     else:
-        return np.abs(score(weights, bias, features))
+        scored = score(weights, bias, features)
+        return np.abs(scored)
 
+# оцениваем ошибку перцептрона
 def mean_perceptron_error(weights, bias, features, labels):
     total_error = 0
     for i in range(len(features)):
@@ -31,10 +35,12 @@ def mean_perceptron_error(weights, bias, features, labels):
     return total_error/len(features)
 
 # Shorter version of the perceptron trick
-def perceptron_trick(weights, bias, features, label, learning_rate = 0.01):
-    pred = prediction(weights, bias, features)
+def perceptron_trick(weights, bias, point_coordinates, label, learning_rate = 0.01):
+    #делаем предсказние значения в точке, если скаляное произведени двух весов и двух координат плюс bias больше 0, то 1 иначе 0
+    pred = prediction(weights, bias, point_coordinates)
+    #вычисляем новые коеффициенты и точку пересечения с осью.
     for i in range(len(weights)):
-        weights[i] += (label-pred)*features[i]*learning_rate
+        weights[i] += (label-pred) * point_coordinates[i] * learning_rate
         bias += (label-pred)*learning_rate
     return weights, bias
 
@@ -47,7 +53,9 @@ def perceptron_algorithm(features, labels, learning_rate = 0.01, epochs = 200):
         utils.draw_line(weights[0], weights[1], bias, color='grey', linewidth=1.0, linestyle='dotted')
         error = mean_perceptron_error(weights, bias, features, labels)
         errors.append(error)
+        #получаем случайный индекс из диапазона длины массива точек.
         i = random.randint(0, len(features)-1)
+        #в точке выбранной случайно делаем предсказание и корректируем коэффициенты. коэффициентов 2, координат в точке 2
         weights, bias = perceptron_trick(weights, bias, features[i], labels[i])
     utils.draw_line(weights[0], weights[1], bias)
     utils.plot_points(features, labels)
@@ -56,6 +64,7 @@ def perceptron_algorithm(features, labels, learning_rate = 0.01, epochs = 200):
     return weights, bias
 
 features = np.array([[1,0],[0,2],[1,1],[1,2],[1,3],[2,2],[2,3],[3,2]])
+# meanings
 labels = np.array([0,0,0,0,1,1,1,1])
 
 # Plotting the points
@@ -66,10 +75,7 @@ utils.plot_points(features, labels)
 
 #weights = [1,1]
 #bias = -3.5
-weights = [1,2]
 bias = -4
 
-pyplot.show()
 random.seed(0)
-
 perceptron_algorithm(features, labels)
